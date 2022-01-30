43dd2a308ac5c089a4ac5f994e3c3bc09eba4c69
Kizito007 <nwakakelechi49@gmail.com>
2022-01-30 17:42:51 +0100

    type assertion

---
+++ dist/index.js
@@ -0,0 +1,52 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = "KiziCorp";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
+//Tuple : for type specification of arrays
+let person = [1, "Two", true];
+//Tuple Array
+let employee;
+employee = [
+    [1, "Brad"],
+    [2, "John"],
+    [3, "Jill"],
+];
+// Union
+let pid = 22;
+pid = "22";
+// Emum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: "John"
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+// Interfaces
